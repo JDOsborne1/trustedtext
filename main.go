e4ec74c37061c6091628ee22ba8eae1133472652
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-04 21:29:34 +0100

    Setting up a possibly useful interface

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import "fmt"
 
-type TT_element interface {
-
+type tt_interface interface {
+	Instantiate(_author string, _keys []string, _body string) tt_interface
+	Edit(_existing_element tt_interface, _new_body string )
 }
 
-type tt_element struct {
+type tt_struct struct {
 	author string
 	keys []string
 	body string
 }
 
-func Instantiate_tt(_author string, _keys []string, _body string) tt_element {
-	return tt_element{author: _author, keys: _keys, body: _body}
+func Instantiate(_author string, _keys []string, _body string) tt_struct {
+	return tt_struct{author: _author, keys: _keys, body: _body}
 }
 
 func main() {
-	test_tt := Instantiate_tt(
+	test_tt := Instantiate(
 		"Johnny Bravo",
 		[]string{"diary", "test"},
 		"This is the best and worst day ever, and I'm glad I can record it in a trusted way",
