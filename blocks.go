d227dbddcb0f578291a5af2f9b2b189ff1719c35
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-04 22:17:32 +0100

    Implementing dummy signing

--- blocks.go
+++ blocks.go
@@ -4,10 +4,22 @@ type trustedtext_s struct {
 	author string
 	tags   []string
 	body   string
+	hash   string
 }
 
 func Instantiate(_author string, _tags []string, _body string) trustedtext_s {
-	return trustedtext_s{author: _author, tags: _tags, body: _body}
+	unsigned_tt := trustedtext_s{author: _author, tags: _tags, body: _body}
+	return sign_tt(unsigned_tt)
+}
+
+func sign_tt(_existing_trustedtext trustedtext_s) trustedtext_s {
+	content_hash := return_hash(_existing_trustedtext)
+	_existing_trustedtext.hash = content_hash
+	return _existing_trustedtext
+}
+
+func return_hash(_trusted_text_element trustedtext_s) string {
+	return "testhash"
 }
 
 func Edit(_existing_element trustedtext_s, _new_author string, _new_body string) trustedtext_s {
