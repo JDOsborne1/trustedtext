338503b2fd81f118e29fa01ba0c5ca91367f9f8a
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-05 14:09:45 +0100

    Ignoring new error handling in main loop

--- main.go
+++ main.go
@@ -5,19 +5,19 @@ import "fmt"
 
 
 func main() {
-	test_ttc := Genesis(
+	test_ttc, _ := Genesis(
 		"Johnny Bravo",
 		[]string{"diary", "test"},
 	)
 	fmt.Println(test_ttc)
-	test_ttc = Amend(
+	test_ttc, _ = Amend(
 		test_ttc,
 		"Johnny Bravo",
 		"This is the best and worst day ever, and I'm glad I can record it in a trusted way",
 	)
 	fmt.Println(test_ttc)
 
-	test_ttc = Amend(
+	test_ttc, _ = Amend(
 		test_ttc, 
 		"Johnny Bravo", 
 		"Actually, now I've recorded it, it's simply the best day",
