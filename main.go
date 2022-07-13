d06ef642208a33511ef683d14dd77b7545e8c1da
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-13 22:36:42 +0100

    experimenting with http sharing

--- main.go
+++ main.go
@@ -1,12 +1,97 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	// "html"
+	"log"
+	"net/http"
 
+	"golang.org/x/exp/maps"
 )
 
+var test_chain trustedtext_chain_s
+const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
+const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
+
+
+
+func give_block(w http.ResponseWriter, r *http.Request) {
+	parsed_q := r.URL.Query()
+	requested_hash := parsed_q["block_hash"][0]
+	requested_block := test_chain.tt_chain[requested_hash]
+	text_block, _ := json.Marshal(requested_block)
+	fmt.Fprint(w, string(text_block))
+}
+
+func give_known_blocks(w http.ResponseWriter, r *http.Request) {
+	output_encoder := json.NewEncoder(w)
+	output_encoder.Encode(maps.Keys(test_chain.tt_chain))
+}
+
+
+func test_handle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	var post_deposit []byte
+	var err error
+	var n int
+	n, err = r.Body.Read(post_deposit)
+	if n == 0 {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "r.Body: %v\n", r.Body)
+		fmt.Fprint(w, "No bytes read")
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+	}
+	// resultant_block := &trustedtext_s{}
+	// err = json.Unmarshal(post_deposit, resultant_block)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	fmt.Fprint(w, err)
+	// }
+	
+	// text_block, err := json.Marshal(resultant_block)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	fmt.Fprint(w, err)
+	// }
+	fmt.Fprint(w, string(post_deposit))
+	
+}
 
 
 func main() {
 
+	test_chain, _ = Genesis(
+		test_pub_key,
+		[]string{"test", "not-a-blog"},
+		test_pri_key,
+	)
+	test_block_1, _ := Instantiate(
+		test_pub_key,
+		tt_body{
+			Instruction_type: "publish",
+			Instruction: "My First ever message",
+		},
+		test_pri_key,
+	)
+	test_block_2, _ := Instantiate(
+		test_pub_key,
+		tt_body{
+			Instruction_type: "publish",
+			Instruction: "My Second ever message",
+		},
+		test_pri_key,
+	)
+	test_chain, _ = Process_incoming_block(test_chain, test_block_1)
+	test_chain, _ = Process_incoming_block(test_chain, test_block_2)
 
+	http.HandleFunc("/block", give_block)
+	http.HandleFunc("/known_blocks", give_known_blocks)
+	http.HandleFunc("/test", test_handle)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
