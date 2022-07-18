2e1c1a5b8934afe8bc08e6f6796f859fcec60156
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-18 11:20:49 +0100

    Structuring the main function better, to abstract the block extraction

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+
 	// "html"
 	"log"
 	"net/http"
@@ -13,36 +15,42 @@ const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d
 const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 
 
+func extract_submitted_block(r *http.Request) (trustedtext_s, error) {
+	var post_deposit []byte
+	var err error
+	post_deposit, err = ioutil.ReadAll(r.Body)
+	if err != nil {
+		return trustedtext_s{}, err
+	}
+	resultant_block := &trustedtext_s{}
+	err = json.Unmarshal(post_deposit, resultant_block)
+	if err != nil {
+		return trustedtext_s{}, err
+	}
+	return *resultant_block, nil
+}
 
-
-
+ 
 
 func test_handle(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	var post_deposit []byte
-	var err error
-	post_deposit, err = ioutil.ReadAll(r.Body)
+	resultant_block, err := extract_submitted_block(r)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, err)
 	}
-	// resultant_block := &trustedtext_s{}
-	// err = json.Unmarshal(post_deposit, resultant_block)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	fmt.Fprint(w, err)
-	// }
 	
+
 	
-	// text_block, err := json.Marshal(resultant_block)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	fmt.Fprint(w, err)
-	// }
-	fmt.Fprint(w, string(post_deposit))
+	text_block, err := json.Marshal(resultant_block)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+	}
+	fmt.Fprint(w, string(text_block))
 	
 }
 
