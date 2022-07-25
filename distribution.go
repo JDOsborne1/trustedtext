ab524eb9183790dd4bd85e4ab5764fb3e2560dcb
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-25 23:28:49 +0100

    Moving distribution handling to dedicated file

--- distribution.go
+++ distribution.go
@@ -1,12 +1,17 @@
 package main
 
-import "golang.org/x/exp/maps"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
 
+	"golang.org/x/exp/maps"
+)
 
-
-// Fork_chain_essentials takes an existing trusted text chain, and generates a copy of it. 
-// This copy of the 'essentials' only takes the elements of the head_hash_tree with it, 
-// producing the effect of preserving and copying the core progression of the trusted text 
+// Fork_chain_essentials takes an existing trusted text chain, and generates a copy of it.
+// This copy of the 'essentials' only takes the elements of the head_hash_tree with it,
+// producing the effect of preserving and copying the core progression of the trusted text
 // elements, and ignoring any un-promoted blocks
 func Fork_chain_essentials(_trusted_text_chain trustedtext_chain_s) trustedtext_chain_s {
 	essential_keys := maps.Keys(_trusted_text_chain.head_hash_tree)
@@ -28,3 +33,67 @@ func Is_hash_in_chain(_trusted_text_chain trustedtext_chain_s, _comparison_hash 
 	return check_map[_comparison_hash]
 }
 
+
+func check_with_peers() error{
+	if len(peerlist) == 0 {
+		return errors.New("cant validate against empty peerlist")
+	}
+	missing_blocks := []string{}
+	for _, peer := range peerlist {
+		peers_missing_blocks, err := check_with_a_peer(peer)
+		if err != nil {
+			return err
+		}
+		for _, block := range peers_missing_blocks {
+			retrieve_from_a_peer(peer, block)
+		}
+		missing_blocks = append(missing_blocks, peers_missing_blocks...)
+	}
+
+	if len(missing_blocks) != 0 {
+		fmt.Println("Missing items on the web")
+	}
+	return nil
+}
+
+func check_with_a_peer(peer peer_detail) ([]string, error) {
+
+	// Get and decode known blocks
+	resp, err := http.Get("http://" + peerlist[0].Path + "/known_blocks")
+	if err != nil {
+		return []string{}, err
+	}
+	response_decoder := json.NewDecoder(resp.Body)
+	known_blocks_of_peer := &[]string{}
+	response_decoder.Decode(known_blocks_of_peer)
+
+	// Determine missing elements
+	peer_blocks_map := util_make_boolean_map_from_slice(*known_blocks_of_peer)
+	my_blocks := maps.Keys(test_chain.tt_chain)
+
+	new_keys_of_peer := util_anti_set_map(peer_blocks_map, my_blocks)
+
+	return maps.Keys(new_keys_of_peer), nil
+}
+
+func retrieve_from_a_peer(peer peer_detail, block_hash string) error {
+	resp, err := http.Get("http://" + peer.Path + "/block" + "?block_hash=" + block_hash)
+	if err != nil {
+		return err
+	}
+	response_decoder := json.NewDecoder(resp.Body) 
+	returned_block := &trustedtext_s{}
+
+	err = response_decoder.Decode(returned_block)
+	if err != nil {
+		return err
+	}
+
+	test_chain, err = Process_incoming_block(test_chain, *returned_block)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+
+}
--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
+
 	"fmt"
 	"log"
 	"net/http"
-
-	"golang.org/x/exp/maps"
 )
 
 var peerlist []peer_detail
@@ -15,72 +12,6 @@ var test_chain trustedtext_chain_s
 const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 
-func check_with_peers() error{
-	if len(peerlist) == 0 {
-		return errors.New("cant validate against empty peerlist")
-	}
-	missing_blocks := []string{}
-	for _, peer := range peerlist {
-		peers_missing_blocks, err := check_with_a_peer(peer)
-		if err != nil {
-			return err
-		}
-		for _, block := range peers_missing_blocks {
-			retrieve_from_a_peer(peer, block)
-		}
-		missing_blocks = append(missing_blocks, peers_missing_blocks...)
-	}
-
-	if len(missing_blocks) != 0 {
-		fmt.Println("Missing items on the web")
-	}
-	return nil
-}
-
-func check_with_a_peer(peer peer_detail) ([]string, error) {
-	// Logging step
-	fmt.Println("Checking the blocks held by", peer.Claimed_name)
-
-	// Get and decode known blocks
-	resp, err := http.Get("http://" + peerlist[0].Path + "/known_blocks")
-	if err != nil {
-		return []string{}, err
-	}
-	response_decoder := json.NewDecoder(resp.Body)
-	known_blocks_of_peer := &[]string{}
-	response_decoder.Decode(known_blocks_of_peer)
-
-	// Determine missing elements
-	peer_blocks_map := util_make_boolean_map_from_slice(*known_blocks_of_peer)
-	my_blocks := maps.Keys(test_chain.tt_chain)
-
-	new_keys_of_peer := util_anti_set_map(peer_blocks_map, my_blocks)
-
-	return maps.Keys(new_keys_of_peer), nil
-}
-
-func retrieve_from_a_peer(peer peer_detail, block_hash string) error {
-	fmt.Println("Retrieving", block_hash, "from", peer.Claimed_name)
-	resp, err := http.Get("http://" + peer.Path + "/block" + "?block_hash=" + block_hash)
-	if err != nil {
-		return err
-	}
-	response_decoder := json.NewDecoder(resp.Body) 
-	returned_block := &trustedtext_s{}
-
-	err = response_decoder.Decode(returned_block)
-	if err != nil {
-		return err
-	}
-
-	test_chain, err = Process_incoming_block(test_chain, *returned_block)
-	if err != nil {
-		return nil
-	}
-
-	return nil
-
-}
 
 func test_handler(w http.ResponseWriter, r *http.Request) {
 	err := check_with_peers()
