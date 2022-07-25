6a80fd189666c52ed29b78070d9e2c782d10481a
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-26 00:50:59 +0100

    adding in shift path helper, as per Axel Wagner

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 )
 
 
@@ -29,7 +31,16 @@ type config_struct struct {
 	Chain_path string
 }
 
-
+// shift_path splits the given path into the first segment (head) and
+// the rest (tail). For example, "/foo/bar/baz" gives "foo", "/bar/baz".
+func shift_path(p string) (head, tail string) {
+    p = path.Clean("/" + p)
+    i := strings.Index(p[1:], "/") + 1
+    if i <= 0 {
+        return p[1:], "/"
+    }
+    return p[1:i], p[i:]
+}
 
 func main() {
 
