abcc6a2504838c6c3554a80d94b40f330a48e0ae
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-26 00:32:33 +0100

    abstracting the test chain into files also

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 
 
 var test_chain trustedtext_chain_s
-const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
-const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
+// const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
+// const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 const default_config_path = "config.json"
 
 func test_handler(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +28,11 @@ func test_handler(w http.ResponseWriter, r *http.Request) {
 
 type config_struct struct {
 	Peerlist_path string 
+	Chain_path string
 }
 
-func read_peerlist(config config_struct) ([]peer_detail, error) {
-	bytefile, err := ioutil.ReadFile(config.Peerlist_path)
+func read_peerlist(_config config_struct) ([]peer_detail, error) {
+	bytefile, err := ioutil.ReadFile(_config.Peerlist_path)
 	if err != nil {
 		return []peer_detail{}, err
 	}
@@ -43,13 +44,13 @@ func read_peerlist(config config_struct) ([]peer_detail, error) {
 	return *peerlist, nil
 }
 
-func write_peerlist(peerlist []peer_detail, config config_struct) error {
-	marshalled_peerlist, err := json.MarshalIndent(peerlist, "", " ")
+func write_peerlist(_peerlist []peer_detail, _config config_struct) error {
+	marshalled_peerlist, err := json.MarshalIndent(_peerlist, "", " ")
 	if err != nil {
 		return err
 	}
 
-	err = ioutil.WriteFile(config.Peerlist_path, marshalled_peerlist, 0644) 
+	err = ioutil.WriteFile(_config.Peerlist_path, marshalled_peerlist, 0644) 
 	
 	if err != nil {
 		return err
@@ -92,31 +93,35 @@ func read_config(_config_path string) (config_struct, error) {
 	return *config, nil
 }
 
+func write_chain(_chain trustedtext_chain_s, _config config_struct) error {
+	marshalled_chain, err := json.MarshalIndent(_chain, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(_config.Chain_path, marshalled_chain, 0644)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func read_chain(_config config_struct) (trustedtext_chain_s, error) {
+	bytefile, err := ioutil.ReadFile(_config.Chain_path)
+	if err != nil {
+		return trustedtext_chain_s{}, err
+	}
+	chain := &trustedtext_chain_s{}
+	err = json.Unmarshal(bytefile, chain)
+	if err != nil {
+		return trustedtext_chain_s{}, err
+	}
+	return *chain, nil
+}
+
 func main() {
 
-	test_chain, _ = Genesis(
-		test_pub_key,
-		[]string{"test", "not-a-blog"},
-		test_pri_key,
-	)
-	test_block_1, _ := Instantiate(
-		test_pub_key,
-		tt_body{
-			Instruction_type: "publish",
-			Instruction: "My First ever message",
-		},
-		test_pri_key,
-	)
-	test_chain, _ = Process_incoming_block(test_chain, test_block_1)
-	test_block_2, _ := Instantiate(
-		test_pub_key,
-		tt_body{
-			Instruction_type: "publish",
-			Instruction: "My Second ever message",
-		},
-		test_pri_key,
-	)
-	test_chain, _ = Process_incoming_block(test_chain, test_block_2)
+	used_config, _ := read_config(default_config_path)
+	test_chain, _ = read_chain(used_config)
 
 	http.HandleFunc("/block", give_block)
 	http.HandleFunc("/known_blocks", give_known_blocks)
