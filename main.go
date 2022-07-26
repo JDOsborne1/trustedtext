cf5bcd3ca3e83867dc0139c94e50edc21ec02e68
Joseph Osborne <jd.osborne5@icloud.com>
2022-07-26 01:16:52 +0100

    refactoring handlers for single blocks

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path"
@@ -36,22 +38,67 @@ type generic_handler struct {
 }
 
 func (generic_handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Full path is: " + r.URL.Path)
 	var head string
 	head, r.URL.Path = shift_path(r.URL.Path)
-	fmt.Fprintf(w, "head of path is: " + head)
-	fmt.Fprint(w, "the tail of path is: " + r.URL.Path )
-	
+	if head == "block" {
+		block_handler(w, r)
+	}
 }
+
+func block_handler(w http.ResponseWriter, r *http.Request) {
+	var head string
+	head, r.URL.Path = shift_path(r.URL.Path)
+	if r.Method == "GET" {
+		give_block(w, r, head)
+	}
+	if r.Method == "POST" {
+		submit_block(w, r)
+	}
+}
+
+func give_block(w http.ResponseWriter, r *http.Request, _block_hash string) {
+	requested_block, _ := Return_specified_hash(test_chain, _block_hash)
+	text_block, _ := json.Marshal(requested_block)
+	fmt.Fprint(w, string(text_block))
+}
+
+func submit_block(w http.ResponseWriter, r *http.Request) {
+	var post_deposit []byte
+	var err error
+	post_deposit, err = ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+	resultant_block := &trustedtext_s{}
+	err = json.Unmarshal(post_deposit, resultant_block)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	new_chain, err := Process_incoming_block(test_chain, *resultant_block)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+	if err != nil {
+		test_chain = new_chain
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
 
 func main() {
 
 	used_config, _ := read_config(default_config_path)
 	test_chain, _ = read_chain(used_config)
 
-	http.HandleFunc("/block", give_block)
 	http.HandleFunc("/known_blocks", give_known_blocks)
-	http.HandleFunc("/submit_block", submit_block)
 	http.HandleFunc("/share_peerlist", share_peerlist)
 	http.HandleFunc("/add_peer", add_peer)
 	http.HandleFunc("/check_with_peers", peer_check_handler)
--- route_handlers.go
+++ route_handlers.go
@@ -14,51 +14,10 @@ type peer_detail struct {
 	Path         string
 }
 
-func give_block(w http.ResponseWriter, r *http.Request) {
-	parsed_q := r.URL.Query()
-	requested_hash := parsed_q["block_hash"][0]
-	requested_block, _ := Return_specified_hash(test_chain, requested_hash)
-	text_block, _ := json.Marshal(requested_block)
-	fmt.Fprint(w, string(text_block))
-}
 
 func give_known_blocks(w http.ResponseWriter, r *http.Request) {
 	output_encoder := json.NewEncoder(w)
 	output_encoder.Encode(maps.Keys(test_chain.Tt_chain))
-}
-
-func submit_block(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	var post_deposit []byte
-	var err error
-	post_deposit, err = ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err)
-		return
-	}
-	resultant_block := &trustedtext_s{}
-	err = json.Unmarshal(post_deposit, resultant_block)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	new_chain, err := Process_incoming_block(test_chain, *resultant_block)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err)
-		return
-	}
-	if err != nil {
-		test_chain = new_chain
-	}
-
-	w.WriteHeader(http.StatusCreated)
 }
 
 func share_peerlist(w http.ResponseWriter, r *http.Request) {
