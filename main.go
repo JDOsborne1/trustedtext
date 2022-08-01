782b4d5f9463735de1ad7776344716af95c52b3c
Joseph Osborne <jd.osborne5@icloud.com>
2022-08-01 18:45:38 +0100

    abstracting the window generator

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
-
 
 // const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 // const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
@@ -60,30 +61,34 @@ func announce_block_generation(_instruction_type string, _instruction_body strin
 }
 	
 
-	
-	
-func main() {
-	go webservice_main()
-
-	tt_app := app.New()
-	main_window := tt_app.NewWindow("Block Generator window")
+func block_generator_window(_app_to_launch_in fyne.App) fyne.Window {
+	main_window := _app_to_launch_in.NewWindow("Block Generator window")
 	main_window.SetFullScreen(true)
-
+	
 	body_input := widget.NewMultiLineEntry()
 	body_input.SetMinRowsVisible(10)
-
+	
 	private_key_input := widget.NewPasswordEntry()
 	private_key_input.SetPlaceHolder("private key")
 	public_key_input := widget.NewEntry()
 	public_key_input.SetPlaceHolder("public key")
-
-	save_button := widget.NewButton("Save", func() {announce_block_generation("publish", body_input.Text, public_key_input.Text, private_key_input.Text)})
-
 	
-
+	save_button := widget.NewButton("Save", func() {announce_block_generation("publish", body_input.Text, public_key_input.Text, private_key_input.Text)})
 	content := container.NewVBox(body_input, private_key_input, public_key_input, save_button)
-
-
+	
 	main_window.SetContent(content)
+
+	return main_window
+}
+	
+func local_app_main() {
+	tt_app := app.New()
+	
+	main_window := block_generator_window(tt_app)
 	main_window.ShowAndRun()
+
+}	
+func main() {
+	go webservice_main()
+	local_app_main()
 }
