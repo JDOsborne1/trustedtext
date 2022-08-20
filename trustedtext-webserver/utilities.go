c5aa35ca5d099acdfc0f739f3500f4012fc01f36
Joseph Osborne <jd.osborne5@icloud.com>
2022-08-20 08:39:14 +0100

    undoing previous change as it didn't do as intended

--- trustedtext-webserver/utilities.go
+++ trustedtext-webserver/utilities.go
@@ -7,7 +7,7 @@ import (
 
 func util_error_wrapper(_response_writer http.ResponseWriter, _possible_error error) {
 	if _possible_error != nil {
-		fmt.Fprint(_response_writer,  _possible_error)
 		_response_writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(_response_writer,  _possible_error)
 	}
 }
