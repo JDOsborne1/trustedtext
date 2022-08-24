3f7fa1ba1b6ff5868437ab8a0af4f7033e50c009
Joseph Osborne <jd.osborne5@icloud.com>
2022-08-25 00:01:48 +0100

    parameterising webservice to support future testing

--- trustedtext-webserver/main.go
+++ trustedtext-webserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,14 +12,14 @@ const default_config_path = "config.json"
 // const test_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 // const test_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 
-func webservice_main() {
+func webservice_main(_port_to_use int) {
 
 	test_handler := new(generic_handler)
 
-	log.Fatal(http.ListenAndServe(":8080", test_handler))
+	log.Fatal(http.ListenAndServe(":" + fmt.Sprint(_port_to_use), test_handler))
 }
 
 func main() {
 
-	webservice_main()
+	webservice_main(8080)
 }
