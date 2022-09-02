1c877ad4d7303393830d094c61fab15d799c1be5
Joseph Osborne <jd.osborne5@icloud.com>
2022-09-02 17:27:33 +0100

    migrating integration tests to use docker compose

--- integration_test_setup.sh
+++ integration_test_setup.sh
@@ -1,4 +1,1 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=tt_test))
-
-docker run -d -p 8081:8080 --name first_env tt_test
-docker run -d -p 8082:8080 --name second_env tt_test 
+docker-compose up
