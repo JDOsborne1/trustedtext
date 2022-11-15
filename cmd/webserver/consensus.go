8e299b71836139658eb11986a65fd0ddba9246d0
my_git_username <my_git_username@gmail.com>
2022-11-15 21:50:34 +0000

    format run

--- cmd/webserver/consensus.go
+++ cmd/webserver/consensus.go
@@ -7,11 +7,9 @@ import (
 	"trustedtext"
 )
 
-
 // Borrowing the junk keys from the test scripts to hardcode for now, need to define internal keys later / make keygen public
 const temp_pub_key = "faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
 const temp_pri_key = "366c15a87d86f7a6fe6f7509ecaab3d453f0488b414aef12175a870cc5d1b124faa372113c86e434298d3c2c76c230c41f8ec890d165ef0d124c62758d89a66a"
-
 
 func retrieve_head_hash_from_peer(peer trustedtext.Peer_detail) (string, error) {
 	composed_path := "http://" + peer.Path + "head_block/hash"
